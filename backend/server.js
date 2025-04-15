cb161c012d969f91a588d6fb6cb72307fabcb2e2
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2025-04-15 21:15:55 +0530

    Add backend for room booking

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
+import bookingRoutes from "./routes/booking.js";
 
 dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/bookings", bookingRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://loacalhost:5000");
+  console.log(`Server started at http://localhost:${PORT}`);
 });
